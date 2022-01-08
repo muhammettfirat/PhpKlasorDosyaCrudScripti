5ff8de1b73a93a1c2e8f92d59e66e16d542da6af
muhammettfirat <negrumaquila@gmail.com>
2022-01-09 02:25:57 +0300

    rev4

---
+++ admin/incele.php
@@ -0,0 +1,54 @@
+<?php require_once 'header.php';
+$id=isset($_GET['dosya']) ? (int)$_GET['dosya'] : NULL;
+$dosya= $site->dosya($id);
+?>
+
+<h3>Dosya Detayları</h3>
+<div class="sag_icerik">
+<?php if($id==NULL):?>
+<p>Geçersiz Dosya Yolu</p>
+    <?php else:?>
+
+    <table class="dosyalar">
+        <tr>
+            <td class="sagayasla w150">Dosya Başlığı</td>
+            <td><?php echo $dosya->dosya_baslik?></td>
+        </tr>
+        <tr>
+            <td class="sagayasla w150">Dosya Adı</td>
+            <td><?php echo $dosya->dosya_adi?></td>
+        </tr>
+        <tr>
+            <td class="sagayasla w150">Dosya Boyutu</td>
+            <td><?php echo $site-> dosyaBoyutu($dosya->dosya_boyut)?></td>
+        </tr>
+        <tr>
+            <td class="sagayasla w150">İndirilme Sayısı</td>
+            <td><?php echo $dosya->dosya_indirilme?></td>
+        </tr>
+        <tr>
+            <td class="sagayasla w150">Yükleyen Üye</td>
+            <td><?php echo  $site->uyeAdi($dosya->dosya_yukleyen)->kullanici_adi?></td>
+        </tr>
+        <tr>
+            <td class="sagayasla w150">Eklenme Tarihi</td>
+            <td><?php echo $site->tarih($dosya->dosya_eklenme)?></td>
+        </tr>
+        <tr>
+            <td class="sagayasla w150">Dosya Türü</td>
+            <td><?php echo   $site->dosyaTuru($dosya->dosya_mime)?></td>
+        </tr>
+        <tr>
+            <td class="sagayasla w150">&nbsp;</td>
+            <td>
+                <input type="button" name="indir" value="İndir"/>
+                <input type="button" name="düzenle" value="Düzenle"/>
+                <input type="button" name="sil" value="Sil"/>
+        </td>
+        </tr>
+    </table>
+
+
+        <?php endif;?>
+</div>
+<?php require_once 'footer.php';?>
--- admin/siniflar/site.class.php
+++ admin/siniflar/site.class.php
@@ -12,7 +12,45 @@ parent::__construct();
         $sorgu->execute();
 return $sorgu->fetchAll(PDO::FETCH_OBJ);
     }
-    
-        
-    
+        public function dosya($id){
+            $sorgu=$this->prepare("SELECT * FROM dosyalar WHERE dosya_id = '$id' ");
+            $sorgu->execute();
+    return $sorgu->fetch(PDO::FETCH_OBJ);
+        }
+        public function dosyaBoyutu($byte){
+            if($byte<1024){
+                $byte="$byte BYTE";
+            }elseif($byte<(1024*1024)){
+                $byte= round($byte/1024,2)." KB";
+            }elseif($byte<(1024*1024*1024)){
+                $byte= round($byte/1024*1024,2)." MB";
+            }else{
+                $byte= round($byte/1024*1024*1024,2)." GB";
+            }
+            return $byte;
+        }
+        public function tarih($tarih){
+            $tarih=strtotime($tarih);
+            return date("d.m.Y H:i:s",$tarih);
+        }
+        public function dosyaTuru($turu){
+            switch($turu){
+                
+                case 'application/pdf':$donder='Resim Dosyası (PDF)';break;
+                case 'application/zip':
+                case 'application/x-zip-compressed':    
+                    $donder=' Sıkıştırılmış  ZIP Dosyası (ZIP)';break;
+
+
+                    default:
+                    $donder= 'Bilinmeyen Dosya';
+                    break;
+            }
+            return $donder;
+        }
+        public function uyeAdi($id){
+            $sorgu=$this->prepare("SELECT kullanici_adi FROM kullanicilar WHERE kullanici_id = '$id' ");
+            $sorgu->execute();
+    return $sorgu->fetch(PDO::FETCH_OBJ);
+        }
 }
